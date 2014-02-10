ee862c181a4787dce750df242fc9ef70610b887e
buckket <MrLoom@soeinfachistdas.net>
2014-02-10 19:52:50 +0100

    cleanup

--- pyquake3.py
+++ pyquake3.py
@@ -21,46 +21,55 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import socket
 import re
 
-class Player:
-    def __init__(self, name, frags, ping, address=None, bot=-1):
+
+class Player(object):
+    def __init__(self, name, frags, ping, address=None, bot=False):
         self.name = name
         self.frags = frags
         self.ping = ping
         self.address = address
         self.bot = bot
+
     def __str__(self):
         return self.name
+
     def __repr__(self):
         return str(self)
 
-class PyQuake3:
+
+class PyQuake3(object):
     packet_prefix = '\xff' * 4
-    player_reo = re.compile(r'^(\d+) (\d+) "(.*)"')
+    player_reo = re.compile(r'^(-?\d+) (\d+) "(.*)"')
+
     def __init__(self, server, rcon_password=''):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.set_server(server)
         self.set_rcon_password(rcon_password)
+
     def set_server(self, server):
         try:
             self.address, self.port = server.split(':')
         except:
-            raise Exception('Server address must be in the format of \
-                    "address:port"')
+            raise Exception('Server address must be in the format of "address:port"')
         self.port = int(self.port)
         self.s.connect((self.address, self.port))
+
     def get_address(self):
         return '%s:%s' % (self.address, self.port)
+
     def set_rcon_password(self, rcon_password):
         self.rcon_password = rcon_password
+
     def send_packet(self, data):
         self.s.send('%s%s\n' % (self.packet_prefix, data))
+
     def recv(self, timeout=1):
         self.s.settimeout(timeout)
         try:
             return self.s.recv(4096)
         except socket.error, e:
-            raise Exception('Error receiving the packet: %s' % \
-                    e[1])
+            raise Exception('Error receiving the packet: %s' % e[1])
+
     def command(self, cmd, timeout=1, retries=3):
         while retries:
             self.send_packet(cmd)
@@ -72,12 +81,13 @@ class PyQuake3:
                 return self.parse_packet(data)
             retries -= 1
         raise Exception('Server response timed out')
+
     def rcon(self, cmd):
         r = self.command('rcon "%s" %s' % (self.rcon_password, cmd))
-        if r[1] == 'No rconpassword set on the server.\n' or r[1] == \
-                'Bad rconpassword.\n':
+        if r[1] == 'No rconpassword set on the server.\n' or r[1] == 'Bad rconpassword.\n':
             raise Exception(r[1][:-1])
         return r
+
     def parse_packet(self, data):
         if data.find(self.packet_prefix) != 0:
             raise Exception('Malformed packet')
@@ -85,8 +95,9 @@ class PyQuake3:
         if first_line_length == -1:
             raise Exception('Malformed packet')
         response_type = data[len(self.packet_prefix):first_line_length]
-        response_data = data[first_line_length+1:]
+        response_data = data[first_line_length + 1:]
         return response_type, response_data
+
     def parse_status(self, data):
         split = data[1:].split('\\')
         values = dict(zip(split[::2], split[1::2]))
@@ -99,6 +110,7 @@ class PyQuake3:
             values[var] = split[0]
             self.parse_players(split[1])
         return values
+
     def parse_players(self, data):
         self.players = []
         for player in data.split('\n'):
@@ -110,9 +122,11 @@ class PyQuake3:
                 continue
             frags, ping, name = match.groups()
             self.players.append(Player(name, frags, ping))
+
     def update(self):
         cmd, data = self.command('getstatus')
         self.vars = self.parse_status(data)
+
     def rcon_update(self):
         cmd, data = self.rcon('status')
         lines = data.split('\n')
@@ -128,16 +142,16 @@ class PyQuake3:
             p = p.split(' ')
             self.players.append(Player(p[3][:-2], p[0], p[1], p[5], p[6]))
 
+
 if __name__ == '__main__':
-    q = PyQuake3('localhost:27960','hello')
+    q = PyQuake3('uncloaked.net:27960', 'hello')
     q.update()
     print 'The name of %s is %s, running map %s with %s player(s).' % \
-            (q.get_address(), q.vars['sv_hostname'], \
-            q.vars['mapname'], len(q.players))
+          (q.get_address(), q.vars['sv_hostname'],
+           q.vars['mapname'], len(q.players))
     for player in q.players:
-        print '%s with %s frags and a %sms ping' % (player.name, \
-                player.frags, player.ping)
+        print '%s with %s frags and a %sms ping' % (player.name,
+                                                    player.frags, player.ping)
     q.rcon_update()
     for player in q.players:
         print '%s has an address of %s' % (player.name, player.address)
-
